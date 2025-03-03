0c947f2f6150a480a1fc9580e68ed650b615074e
Lucas Clark <encryptedsalad@gmail.com>
2025-03-02 19:47:04 -0600

    updates to RNN model

--- RNN.py
+++ RNN.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from Tokens import Tokenizer
 from abc import ABC, abstractmethod
@@ -11,12 +10,19 @@ class RNN(ABC, nn.Module):
 
     def __init__(self, tokenizer):
         self.tokenizer = tokenizer
+        
+    @abstractmethod
+    def first_state(self) -> torch.Tensor:
+        """
+        Gets the initial state assuming no input.
+        """
+        pass
     
     @abstractmethod
-    def next_state(cur_token: torch.Tensor) -> torch.Tensor:
+    def next_state(self, cur_token: torch.Tensor) -> torch.Tensor:
         """
         Takes a single input state and produces a single output state. States are arbitrary 1d tensors. 
-        Whoever implements this function is responsible for keeping track of hidden states.
+        Implementer is responsible for keeping track of hidden states.
         """
         pass
 
@@ -27,11 +33,11 @@ class RNN(ABC, nn.Module):
         The input tensor is assumed to be a 2d tensor gotten from torch.stack() of a list of 1d state tensors. 
         The output tensor is a 2d tensor of feeding the batch of input tensors into next_token, and then stacking them.
         """
-        out_states = []
+        out_states = [self.first_state()]
         tokens = batch.unbind()
         for token in tokens:
             out_states.append(self.next_state(token))
-        return torch.stack(out_states)
+        return torch.stack(out_states)[:-1]
     
     @abstractmethod
     def get_token_from_state(self, state: torch.Tensor) -> int:
@@ -48,62 +54,7 @@ class RNN(ABC, nn.Module):
         for _ in range(len):
             cur_state = self.next_state(cur_state)
             out.append(self.get_token_from_state(cur_state))
-        
-
-        
-
-
-class RNNModel(nn.Module):
-    def __init__(self, tokenizer: Tokenizer, hidden_dim: int):
-        super().__init__()
-        self.tokenizer = tokenizer
-        self.num_tokens = tokenizer.num_unique_tokens
-        self.initial_h = nn.Parameter(torch.zeros(hidden_dim))
-        self.W_xh = nn.Parameter(torch.empty(hidden_dim, self.num_tokens))
-        self.W_hh = nn.Parameter(torch.empty(hidden_dim, hidden_dim))
-        self.W_hy = nn.Parameter(torch.empty(self.num_tokens, hidden_dim))
-        self.B_h = nn.Parameter(torch.zeros(hidden_dim))
-        nn.init.xavier_normal_(self.W_xh)
-        nn.init.xavier_normal_(self.W_hh)
-        nn.init.xavier_normal_(self.W_hy)
-        
-    # TODO make this use the vectors that the GRU does
-    def forward(self, token_stream: list[int]):
-        hidden_states = []
-        outputs = []
-        
-        cur_state = self.initial_h.to(device)
-        outputs.append(self.W_hy @ cur_state)
-        
-        # keep list of hidden states so we aren't modifying h in place
-        for token in token_stream:
-            x = F.one_hot(torch.tensor([token]), self.num_tokens).float().squeeze(0).to(device)
-            x_state = self.W_xh @ x
-            h_state = self.W_hh @ cur_state
-            cur_state = torch.tanh(x_state + h_state + self.B_h)
-            hidden_states.append(cur_state)
-        
-        for hidden_state in hidden_states:
-            outputs.append(self.W_hy @ hidden_state)
-
-        return torch.stack(outputs)
-    
-    def autoregress(self, len: int, temperature = 1.0):
-        cur_state = self.initial_h.to(device)
-        next_token = self.tokenizer.str_to_stream("the quick brown fox")[0]
-        stream = [next_token]
-        for _ in range(len):
-            x = F.one_hot(torch.tensor([next_token]), self.num_tokens).float().squeeze(0).to(device)
-            state_1 = self.W_xh @ x
-            state_2 = self.W_hh @ cur_state
-            cur_state = torch.tanh(state_1 + state_2 + self.B_h).to(device)
-            logits = self.W_hy @ cur_state
-            probs = F.softmax(logits / temperature, dim = 0)
-            next_token = torch.multinomial(probs, 1)
-            stream.append(next_token)
-        
-        return stream
-
+            
     def train_with_data(self, 
             trn_data_path: str, 
             batch_size: int, 
@@ -134,4 +85,4 @@ class RNNModel(nn.Module):
                 print("sample output:  ", end="")
                 with torch.no_grad():
                     print(self.tokenizer.stream_to_str(self.autoregress(100, 0.3)))
-                self.train()
+                self.train()      
